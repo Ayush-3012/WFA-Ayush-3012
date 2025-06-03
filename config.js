b43efa698ba144765bc1bfdbc2bcfb42685c9f64
Ayush-3012 <diveshsrivastava31@gmail.com>
2025-06-03 16:00:50 +0530

    api access

---
+++ config.js
@@ -0,0 +1,3 @@
+export const API_KEY = "a40ea5f742ae9b8b64c23099caa358f4";
+export const API_URL = "https://api.openweathermap.org/data/2.5/";
+export const API_IMAGE_URL = "https://openweathermap.org/img/wn";
